960780eeb38cc7c167270d0e3bbeebecc8d75c0f
gabriel <gabriel.arocha@edu.tbz.ch>
2024-09-19 16:01:41 +0200

    make changes

--- src/components/atoms/AddBlogButton.tsx
+++ src/components/atoms/AddBlogButton.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@mui/material";
 import ButtonProperties from "../../types/ButtonProperties";
 
-function AddBlogPostButton({ onClick, id }: ButtonProperties) {
+function AddBlogPostButton({ onClick }: ButtonProperties) {
     return (
         <Button variant="contained" color="primary" onClick={onClick}>
             +
